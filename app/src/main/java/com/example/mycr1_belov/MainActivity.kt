c28d533191830fd348f0825aed53948a77ef726a
Azazak142 <Azazak142@gmail.com>
2024-03-11 15:12:19 +0300

    added avatar

--- app/src/main/java/com/example/mycr1_belov/MainActivity.kt
+++ app/src/main/java/com/example/mycr1_belov/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.mycr1_belov
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mycr1_belov.ui.theme.Mycr1_BelovTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainWindow()
                 }
             }
         }
@@ -30,17 +41,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainWindow( modifier: Modifier = Modifier) {
+
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(bottom = 0.dp, top = 250.dp, end = 0.dp, start = 0.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ava),
+                contentDescription = "Avatar",
+                modifier = Modifier
+                    .size(100.dp)
+            )
+        }
+
+
+        Text(
+            text = "Hello !",
+            modifier = modifier
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Mycr1_BelovTheme {
-        Greeting("Android")
+        MainWindow()
     }
 }
